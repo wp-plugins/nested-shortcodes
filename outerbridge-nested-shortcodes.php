43718f4a82bdaa13ccddb2fe8e2f117023bc3541
outerbridge <outerbridge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-03 12:41:59 +0000

    v1.0 tested and stable up to WP3.3
    git-svn-id: https://plugins.svn.wordpress.org/nested-shortcodes/trunk@483845 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- outerbridge-nested-shortcodes.php
+++ outerbridge-nested-shortcodes.php
@@ -4,10 +4,18 @@ Plugin Name: Outerbridge Nested Shortcodes
 Plugin URI: http://outerbridge.co.uk/tools
 Description: A small plugin which allows you to use nested shortcodes (i.e. a shortcode within an enclosing shortcode) by implementing a simple "do_shortcode" filter as per the WordPress Codex to content and widgets - see http://codex.wordpress.org/Shortcode_API#Nested_Shortcodes and http://codex.wordpress.org/Function_Reference/do_shortcode
 Author: Mike Jones a.k.a. Outerbridge Mike
-Version: 0.1
+Version: 1.0
 Author URI: http://outerbridge.co.uk
 License: GPL v2
 */
+
+/**
+ *
+ * v1.0 120301 Tested and stable up to WP3.3
+ *
+ * v0.1 110830 Initial release
+ *
+ */
 
 /** use FILTERPRIORITY to change where this operates.
  *  BE CAREFUL though as you may break WordPress by setting this
